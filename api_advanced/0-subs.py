8096758eacc325a3f6d497fe7af62fd3aa0a327d
Leslie Isaro <144017545+l-isaro@users.noreply.github.com>
2024-03-03 21:03:10 +0200

    Update 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,31 +1,19 @@
 #!/usr/bin/python3
-"""Module"""
+"""
+Script that queries subscribers on a given Reddit subreddit.
+"""
 
-import json
 import requests
-import sys
 
 
 def number_of_subscribers(subreddit):
-    """Function that return the number of subscribers from REDDIT API"""
-
+    """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "MyRedditBot/1.0 (by YourUsername)"}
-
-    response = requests.get(url, headers=headers)
-
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
-        subscribers = data["data"]["subscribers"]
+        subscribers = data['data']['subscribers']
         return subscribers
     else:
         return 0
-
-
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        subreddit = sys.argv[1]
-        num_subscribers = number_of_subscribers(subreddit)
-        print(num_subscribers)
